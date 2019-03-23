17ac052a46447fa5a59775a9c1a64e75a628e542
LenaLaRoux <lena.laroux@gmail.com>
2019-03-24 00:00:24 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,52 +1,54 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int number = (int) (Math.random() * 9);
-        boolean isGuessed = false;
-        boolean doNotContinue = false;
-        int answer;
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        int j, k;                                           // счетчики
+        int i = (int) (Math.random() * (words.length - 1)); // три строки являющиеся конструкцией возврата рандомного
+        // элемента массива
+        String wordToGuess = words[i];
+        int wordToGuessLength = wordToGuess.length();
+
+        int maskLength = 15;// присвоение переменной quest рандомного значения массива
+        String mask = String.format("%1$"+maskLength+ "s", "").replace(" ","#"); //Дима, это место чисто для размышления,
+                                                                            // можно и в цикле заполнять, просто чтобы видел другие варианты
+        char[] maskChars = mask.toCharArray();
+
         Scanner scanner = new Scanner(System.in);
 
-        while (!doNotContinue) {
-            for (int attempt = 2; attempt >= 0; attempt--) { // сновной цикл с счетчиком попыток, их свего 3
+        System.out.println("Угадайте какое слово загадано" + Arrays.toString(words)); // вывод элементов массива words
 
-                System.out.print("Угадайте число от 0 до 9: ");
 
-                int userNumber = scanner.nextInt() ; // чтение числа юзера из консоли
-                isGuessed = userNumber == number;
+        String answer;
+        do {                                                // этот цикл должен вернуть на ввод слова
+            System.out.println("Загаданное слово: " + mask);   // Вывод загаданного слова quest
+            System.out.println("Введите слово из перечня");
 
-                if (isGuessed) { // цикл проверки если юзер угадал число
-                    System.out.println("Вы угадали число: " + userNumber);
+            answer = scanner.nextLine();                    // ввод строки пользователем
 
-                } else {
-                    if (userNumber > number) { // условие проверки если введеное число больше
-                        System.out.println("Выше число больше загаданного.");
-                    } else { // условие проверки если введеное число меньше
-                        System.out.println("Ваше число меньше загаданного.");
-                    }
-                }
-                
-                if (attempt == 0){
-                    System.err.println("Игра завершена. Попытки, увы, иcсякли.");
-                }
-                
-                System.out.println("Хотите продолжить? '0' - да, '1' - нет");
-                answer = scanner.nextInt();
-                doNotContinue = answer == 1;
-
-                if (isGuessed || doNotContinue) {
-                    break;
-                } else if (attempt != 0){
-                    System.out.println("У Вас осталось " + attempt + " попыток.");
-                } 
+            if (answer.equals(wordToGuess)) {
+                break;
             }
-        }
-        if (doNotContinue)
-            System.out.println("Увидимся позже.");
+
+
+            for (j = 0; j < answer.length(); j++) {
+                if (wordToGuessLength < j + 1)
+                    break;
+
+                if (answer.charAt(j) == wordToGuess.charAt(j)) {
+                    maskChars[j] = answer.charAt(j);
+                }
+
+            }
+            mask = new String(maskChars);
+
+        } while (true);
+
+        System.err.println("Поздравляю, загаданное слово: " + wordToGuess);
     }
 }
